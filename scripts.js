63e6de62c4a0398b1fbf3b8592ba68f86f5dc436
nm-arealnormalman <130942233+nm-arealnormalman@users.noreply.github.com>
2023-04-17 10:35:29 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById("delete-tweets-btn").addEventListener("click", () => {
+    if (confirm("Are you sure you want to delete all your tweets?")) {
+        deleteTweets();
+    }
+});
+
+function deleteTweets() {
+    // Paste the deleteTweets() function from the previous answer here
+}
