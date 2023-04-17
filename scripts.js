19e869a800a3848a0930a16cb3780387a2597a49
nm-arealnormalman <130942233+nm-arealnormalman@users.noreply.github.com>
2023-04-17 17:01:14 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,12 @@
-async function checkTwitterLogin() {
-    try {
-        const response = await fetch("https://twitter.com/settings/account");
-        if (response.ok) {
-            return true;
-        }
-    } catch (error) {
-        console.error("Error:", error);
-    }
-    return false;
-}
-
 async function loginTwitter() {
     const loginWindow = window.open("https://twitter.com/login", "_blank");
     loginWindow.focus();
 }
 
-async function deleteTweets() {
-    if (!(await checkTwitterLogin())) {
-        document.getElementById("statusMessage").textContent = "Twitterにログインしていません";
-    } else {
-        if (confirm("Are you sure you want to delete all tweets?")) {
-            const deleteInstructions = "1. Go to your Twitter profile.\n2. Open the browser console (Ctrl+Shift+J or Cmd+Opt+J).\n3. Copy and paste the 'delTweets()' function into the console.\n4. Call the function by typing 'delTweets()' and pressing Enter.";
-            alert(`To delete your tweets, please follow these steps:\n\n${deleteInstructions}`);
-        }
+function deleteTweets() {
+    if (confirm("Are you sure you want to delete all tweets?")) {
+        const deleteInstructions = "1. Go to your Twitter profile.\n2. Open the browser console (Ctrl+Shift+J or Cmd+Opt+J).\n3. Copy and paste the 'delTweets()' function into the console.\n4. Call the function by typing 'delTweets()' and pressing Enter.";
+        alert(`To delete your tweets, please follow these steps:\n\n${deleteInstructions}`);
     }
 }
 
