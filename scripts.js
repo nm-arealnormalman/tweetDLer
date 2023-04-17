a505d982dca50b74a30fccf1602cd97076e93ff3
nm-arealnormalman <130942233+nm-arealnormalman@users.noreply.github.com>
2023-04-17 16:10:39 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,45 @@
-document.getElementById("delete-tweets-btn").addEventListener("click", () => {
-    if (confirm("Are you sure you want to delete all your tweets?")) {
-        deleteTweets();
+async function checkTwitterLogin() {
+    try {
+        const response = await fetch("https://twitter.com/settings/account");
+        if (response.ok) {
+            return true;
+        }
+    } catch (error) {
+        console.error("Error:", error);
     }
-});
-
-function deleteTweets() {
-    // Paste the deleteTweets() function from the previous answer here
+    return false;
 }
+
+async function deleteTweets() {
+    if (!(await checkTwitterLogin())) {
+        alert("Please log in to your Twitter account.");
+        location.href = "https://twitter.com/login";
+    } else {
+        if (confirm("Are you sure you want to delete all tweets?")) {
+            location.href = "https://twitter.com/settings/account";
+            delTweets();
+        }
+    }
+}
+
+var delTweets = function () {
+    var tweetsRemaining = document.querySelectorAll('[role="heading"]+div')[1].textContent;
+    console.log('Remaining: ', tweetsRemaining);
+    window.scrollBy(0, 10000);
+    document.querySelectorAll('[aria-label="More"]').forEach(function (v, i, a) {
+        v.click();
+        document.querySelectorAll('span').forEach(function (v2, i2, a2) {
+            if (v2.textContent === 'Delete') {
+                v2.click();
+                document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
+                    v3.click();
+                });
+            } else {
+                document.body.click();
+            }
+        });
+    });
+    setTimeout(delTweets, 4000);
+}
+
+document.getElementById("deleteTweets").addEventListener("click", deleteTweets);
