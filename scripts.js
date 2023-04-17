1482a4ffe50f058cf941ed1deaef016a7b1e3cc2
nm-arealnormalman <130942233+nm-arealnormalman@users.noreply.github.com>
2023-04-17 16:56:13 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,11 +10,14 @@ async function checkTwitterLogin() {
     return false;
 }
 
+async function loginTwitter() {
+    const loginWindow = window.open("https://twitter.com/login", "_blank");
+    loginWindow.focus();
+}
+
 async function deleteTweets() {
     if (!(await checkTwitterLogin())) {
-        alert("Please log in to your Twitter account.");
-        const loginWindow = window.open("https://twitter.com/login", "_blank");
-        loginWindow.focus();
+        document.getElementById("statusMessage").textContent = "Twitterにログインしていません";
     } else {
         if (confirm("Are you sure you want to delete all tweets?")) {
             const deleteInstructions = "1. Go to your Twitter profile.\n2. Open the browser console (Ctrl+Shift+J or Cmd+Opt+J).\n3. Copy and paste the 'delTweets()' function into the console.\n4. Call the function by typing 'delTweets()' and pressing Enter.";
@@ -23,24 +26,5 @@ async function deleteTweets() {
     }
 }
 
-var delTweets = function () {
-    var tweetsRemaining = document.querySelectorAll('[role="heading"]+div')[1].textContent;
-    console.log('Remaining: ', tweetsRemaining);
-    window.scrollBy(0, 10000);
-    document.querySelectorAll('[aria-label="More"]').forEach(function (v, i, a) {
-        v.click();
-        document.querySelectorAll('span').forEach(function (v2, i2, a2) {
-            if (v2.textContent === 'Delete') {
-                v2.click();
-                document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
-                    v3.click();
-                });
-            } else {
-                document.body.click();
-            }
-        });
-    });
-    setTimeout(delTweets, 4000);
-}
-
+document.getElementById("loginTwitter").addEventListener("click", loginTwitter);
 document.getElementById("deleteTweets").addEventListener("click", deleteTweets);
